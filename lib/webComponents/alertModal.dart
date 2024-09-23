3d4b6deaf047f4c9b4b9cd1536e3539a307671c0
Juan José Arteta <77931977+jj131204@users.noreply.github.com>
2024-09-23 17:47:28 -0500

    pruebas

--- lib/webComponents/alertModal.dart
+++ lib/webComponents/alertModal.dart
@@ -73,131 +73,136 @@ class _CustomModalState extends State<CustomModal> {
 
     final rutaImagen = widget.imagen;
 
-    return Center(
-      child: Container(
-          constraints: containerStyle.customBoxConstraints(context),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(8.0),
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                      flex: 1, // Ocupa 33% del ancho del Row
-                      child: Image.asset(rutaImagen)
-                      // Image.asset('assets/Recurso 1-8.png'),
+    return MediaQuery(
+      data: MediaQuery.of(context).copyWith(
+        textScaler: TextScaler.linear(1.0),  // Usando textScaler en lugar de textScaleFactor
+      ),
+      child: Center(
+        child: Container(
+            constraints: containerStyle.customBoxConstraints(context),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                        flex: 1, // Ocupa 33% del ancho del Row
+                        child: Image.asset(rutaImagen)
+                        // Image.asset('assets/Recurso 1-8.png'),
+                        ),
+                    SizedBox(width: 10.0),
+                    Expanded(
+                      flex: 2, // Ocupa el doble del ancho de la imagen (66%)
+                      child: Container(
+                        constraints:
+                            containerStyle.customBoxConstraints2(context),
+                        padding: EdgeInsets.only(
+                            top: 20, left: 0, right: 20, bottom: 10),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Text(
+                              widget.title,
+                              style: TextStyle(
+                                  fontFamily: "NunitoBold",
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color(0xff707070)),
+                              textAlign: TextAlign.right,
+                            ),
+                            SizedBox(height: 25.0),
+                            Text(widget.descripcion,
+                                style: TextStyle(
+                                    fontFamily: "NunitoRegular",
+                                    fontSize: 18.0,
+                                    fontWeight: FontWeight.normal,
+                                    color: Color(0xff707070)),
+                                textAlign: TextAlign.right),
+                            SizedBox(height: 20.0),
+                          ],
+                        ),
                       ),
-                  SizedBox(width: 10.0),
-                  Expanded(
-                    flex: 2, // Ocupa el doble del ancho de la imagen (66%)
-                    child: Container(
-                      constraints:
-                          containerStyle.customBoxConstraints2(context),
-                      padding: EdgeInsets.only(
-                          top: 20, left: 0, right: 20, bottom: 10),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          Text(
-                            widget.title,
-                            style: TextStyle(
+                    ),
+                    // Image.asset("assets/Recurso 1-8.png", width:150.0 ),
+                  ],
+                ),
+                Container(
+                  padding:
+                      EdgeInsets.only(top: 0, left: 0, right: 20, bottom: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      // displayButton1(widget.funcion1, widget.textoboton1),
+      
+                      Visibility(
+                        visible: boton1,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Color(0xff000000),
+                              padding: EdgeInsets.only(
+                                  top: 6, left: 6, right: 6, bottom: 6),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              )
+                            ),
+                            onPressed: () {
+                              widget.funcion1(context);
+                            },
+                            child: Text(widget.textoboton1,
+                              style: TextStyle(
                                 fontFamily: "NunitoBold",
                                 fontSize: 20.0,
                                 fontWeight: FontWeight.bold,
-                                color: Color(0xff707070)),
-                            textAlign: TextAlign.right,
-                          ),
-                          SizedBox(height: 25.0),
-                          Text(widget.descripcion,
-                              style: TextStyle(
-                                  fontFamily: "NunitoRegular",
-                                  fontSize: 18.0,
-                                  fontWeight: FontWeight.normal,
-                                  color: Color(0xff707070)),
-                              textAlign: TextAlign.right),
-                          SizedBox(height: 20.0),
-                        ],
-                      ),
-                    ),
-                  ),
-                  // Image.asset("assets/Recurso 1-8.png", width:150.0 ),
-                ],
-              ),
-              Container(
-                padding:
-                    EdgeInsets.only(top: 0, left: 0, right: 20, bottom: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    // displayButton1(widget.funcion1, widget.textoboton1),
-
-                    Visibility(
-                      visible: boton1,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: Color(0xff000000),
-                            padding: EdgeInsets.only(
-                                top: 6, left: 6, right: 6, bottom: 6),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20),
-                            )
-                          ),
-                          onPressed: () {
-                            widget.funcion1(context);
-                          },
-                          child: Text(widget.textoboton1,
-                            style: TextStyle(
-                              fontFamily: "NunitoBold",
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.bold,
-                              color: Color(0xffffffff)
-                            ),
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ),
-                      ),
-                    
-
-                    SizedBox(width: 20.0),
-
-                    Expanded(
-                      child: Visibility(
-                        visible: boton2,
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              backgroundColor: Color(0xff62C2FE),
-                              padding: EdgeInsets.only(
-                                  top: 12, left: 12, right: 12, bottom: 12),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20),
-                              )),
-                          onPressed: () {
-                            print(widget.funcion2);
-                            // alertmodalclose(context);s
-                            widget.funcion2(context);
-                          },
-                          child: Text(widget.textoboton2,
-                            style: TextStyle(
-                              fontFamily: "NunitoBold",
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.bold,
-                              color: Color(0xffffffff)),
+                                color: Color(0xffffffff)
+                              ),
                               overflow: TextOverflow.ellipsis,
                             ),
-
+                          ),
+                        ),
+                      
+      
+                      SizedBox(width: 20.0),
+      
+                      Expanded(
+                        child: Visibility(
+                          visible: boton2,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Color(0xff62C2FE),
+                                padding: EdgeInsets.only(
+                                    top: 6, left: 6, right: 6, bottom: 6),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                )),
+                            onPressed: () {
+                              print(widget.funcion2);
+                              // alertmodalclose(context);s
+                              widget.funcion2(context);
+                            },
+                            child: Text(widget.textoboton2,
+                              style: TextStyle(
+                                fontFamily: "NunitoBold",
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xffffffff)),
+                                overflow: TextOverflow.ellipsis,
+                              ),
+      
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          )),
+                    ],
+                  ),
+                )
+              ],
+            )),
+      ),
     );
   }
 }
